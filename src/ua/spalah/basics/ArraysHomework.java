d0531bd2bfd6a06172b39e8a27a4b766606a39d7
dmitry.dev <dmitry.dev@gmail.com>
2016-12-14 19:48:10 +0200

    fix output on "exit"

--- src/ua/spalah/basics/ArraysHomework.java
+++ src/ua/spalah/basics/ArraysHomework.java
@@ -8,42 +8,51 @@ package ua.spalah.basics;
 
         public static void main(String[] args) {
             int arr[] = {1,3,5,6,-7,4,333,-52,11,1};
-            System.out.println(min(arr));
-            System.out.println(max(arr));
+            System.out.println(min(arr, true));
+            System.out.println(max(arr, true));
+            System.out.println(min(arr, false));
+            System.out.println(max(arr, false));
             System.out.println(average(arr));
             printarr(reverse(arr));
         }
 
-        public static int max(int[] array) {
-            for (int i=0; i<array.length; i++)
-            {
-                for (int j = 0; j <array.length-i-1 ; j++)
-                {
-                    if (array[j]>array[j+1])
-                    {
-                        int tmp = array[j];
-                        array[j]=array[j+1];
-                        array[j+1]=tmp;
+        public static int max(int[] array, boolean sort) {
+            if(sort) {
+                for (int i = 0; i < array.length; i++) {
+                    for (int j = 0; j < array.length - i - 1; j++) {
+                        if (array[j] > array[j + 1]) {
+                            int tmp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = tmp;
+                        }
                     }
                 }
+                return array[0];
             }
-            return array[0];
+            else
+            {
+                return 0;
+            }
         }
 
-        public static int min(int[] array) {
-            for (int i=0; i<array.length; i++)
-            {
-                for (int j = 0; j <array.length-i-1 ; j++) {
+        public static int min(int[] array, boolean sort) {
+            if(sort) {
+                for (int i = 0; i < array.length; i++) {
+                    for (int j = 0; j < array.length - i - 1; j++) {
 
-                    if (array[j]<array[j+1])
-                    {
-                        int tmp = array[j];
-                        array[j]=array[j+1];
-                        array[j+1]=tmp;
+                        if (array[j] < array[j + 1]) {
+                            int tmp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = tmp;
+                        }
                     }
                 }
+                return array[0];
             }
-            return array[0];
+            else
+            {
+                return 0;
+            }
         }
 
         public static int average(int[] array) {
